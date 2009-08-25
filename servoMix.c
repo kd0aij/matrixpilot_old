0c291cb9d04fda2a52f7093fa2abf49ece5c176b
Ben Levitt <benjie@gmail.com>
2009-08-25 13:53:36 +0000

    Only compile the mixing code for the chosen airframe type.  This should save a bit of program space.
    Remove a bunch of my unnecessary (long) casts.

--- servoMix.c
+++ servoMix.c
@@ -5,9 +5,12 @@
 //	Perform control based on the airframe type.
 //	Use the radio to determine the baseline pulse widths if the radio is on.
 //	Otherwise, use the trim pulse width measured during power up.
+//
+//	Mix computed roll and pitch controls into the output channels for the compiled airframe type
 
 
 #define REVERSE_IF_NEEDED(NEEDS_REVERSING, VALUE)		((NEEDS_REVERSING) ? (-(VALUE)) : (VALUE))
+
 
 
 void servoMix( void )
@@ -19,73 +22,69 @@ void servoMix( void )
 		for (temp = 0; temp < NUM_INPUTS; temp++)
 			pwIn[temp] = pwTrim[temp];
 	
-	// Mix aileron, elevator, and rudder deflections into output channels for each airframe type
 	
 	// Standard airplane airframe
 	// Mix roll_control and waggle into ailerons
 	// Mix pitch_control into elevators
-	if ( AIRFRAME_TYPE == AIRFRAME_STANDARD )
-	{
-		temp = (long)pwIn[AILERON_INPUT_CHANNEL] + REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, (long)roll_control + (long)waggle) ;
+#if ( AIRFRAME_TYPE == AIRFRAME_STANDARD )
+		temp = pwIn[AILERON_INPUT_CHANNEL] + REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, roll_control + waggle) ;
 		pwOut[AILERON_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		pwOut[AILERON_SECONDARY_OUTPUT_CHANNEL] = (long)3000 +
-			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, (long)pwOut[AILERON_OUTPUT_CHANNEL] - (long)3000) ;
+		pwOut[AILERON_SECONDARY_OUTPUT_CHANNEL] = 3000 +
+			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, pwOut[AILERON_OUTPUT_CHANNEL] - 3000) ;
 		
-		temp = (long)pwIn[ELEVATOR_INPUT_CHANNEL] + REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, (long)pitch_control) ;
+		temp = pwIn[ELEVATOR_INPUT_CHANNEL] + REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, pitch_control) ;
 		pwOut[ELEVATOR_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, (long)yaw_control)*/ ;
+		temp = pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, yaw_control)*/ ;
 		pwOut[RUDDER_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
 		
-		temp = (long)pwIn[THROTTLE_INPUT_CHANNEL] ;
+		temp = pwIn[THROTTLE_INPUT_CHANNEL] ;
 		pwOut[THROTTLE_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
-	}
+#endif
 	
 	
 	// V-Tail airplane airframe
 	// Mix roll_control and waggle into ailerons
 	// Mix pitch_control into both elevator and rudder
-	else if ( AIRFRAME_TYPE == AIRFRAME_VTAIL )
-	{
-		temp = (long)pwIn[AILERON_INPUT_CHANNEL] + REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, (long)roll_control + (long)waggle) ;
+#if ( AIRFRAME_TYPE == AIRFRAME_VTAIL )
+		temp = pwIn[AILERON_INPUT_CHANNEL] + REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, roll_control + waggle) ;
 		pwOut[AILERON_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
 		//	Reverse the polarity of the secondary aileron if necessary
-		pwOut[AILERON_SECONDARY_OUTPUT_CHANNEL] = (long)3000 +
-			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, (long)pwOut[AILERON_OUTPUT_CHANNEL] - (long)3000) ;
+		pwOut[AILERON_SECONDARY_OUTPUT_CHANNEL] = 3000 +
+			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, pwOut[AILERON_OUTPUT_CHANNEL] - 3000) ;
 		
-		temp = (long)pwIn[ELEVATOR_INPUT_CHANNEL] +
-			REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, (long)pitch_control /*+ (long)yaw_control*/) ;
+		temp = pwIn[ELEVATOR_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, pitch_control /*+ yaw_control*/) ;
 		pwOut[ELEVATOR_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[RUDDER_INPUT_CHANNEL] +
-			REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, (long)pitch_control /*- (long)yaw_control*/) ;
+		temp = pwIn[RUDDER_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, pitch_control /*- yaw_control*/) ;
 		pwOut[RUDDER_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[THROTTLE_INPUT_CHANNEL] ;
+		temp = pwIn[THROTTLE_INPUT_CHANNEL] ;
 		pwOut[THROTTLE_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
-	}	
+#endif
 	
 	
 	// Delta-Wing airplane airframe
 	// Mix roll_control, pitch_control, and waggle into aileron and elevator
-	else if ( AIRFRAME_TYPE == AIRFRAME_DELTA )
-	{
-		temp = (long)pwIn[AILERON_INPUT_CHANNEL] +
-			REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, (long)roll_control + (long)pitch_control + (long)waggle) ;
+#if ( AIRFRAME_TYPE == AIRFRAME_DELTA )
+		temp = pwIn[AILERON_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, roll_control + pitch_control + waggle) ;
 		pwOut[AILERON_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[ELEVATOR_INPUT_CHANNEL] +
-			REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, -(long)roll_control + (long)pitch_control - (long)waggle) ;
+		temp = pwIn[ELEVATOR_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, -roll_control + pitch_control - waggle) ;
 		pwOut[ELEVATOR_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, (long)yaw_control)*/ ;
+		temp = pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, yaw_control)*/ ;
 		pwOut[RUDDER_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
 		
-		temp = (long)pwIn[THROTTLE_INPUT_CHANNEL] ;
+		temp = pwIn[THROTTLE_INPUT_CHANNEL] ;
 		pwOut[THROTTLE_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
-	}
+#endif
 	
 	
 	// Helicopter airframe
@@ -93,23 +92,22 @@ void servoMix( void )
 	// Mix full pitch_control into elevator
 	// Ignore waggle for now
 	// FIXME: when one channel saturates, fix other channels to compensate?  Or is this not necessary?
-	else if ( AIRFRAME_TYPE == AIRFRAME_HELI )
-	{
-		temp = (long)pwIn[AILERON_INPUT_CHANNEL] +
-			REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, (long)roll_control/2 + (long)pitch_control/2) ;
+#if ( AIRFRAME_TYPE == AIRFRAME_HELI )
+		temp = pwIn[AILERON_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, roll_control/2 + pitch_control/2) ;
 		pwOut[AILERON_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[ELEVATOR_INPUT_CHANNEL] + REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, (long)pitch_control) ;
+		temp = pwIn[ELEVATOR_INPUT_CHANNEL] + REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, pitch_control) ;
 		pwOut[ELEVATOR_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[AILERON_SECONDARY_OUTPUT_CHANNEL] + 
-			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, -(long)roll_control/2 + (long)pitch_control/2) ;
+		temp = pwIn[AILERON_SECONDARY_OUTPUT_CHANNEL] + 
+			REVERSE_IF_NEEDED(AILERON_SECONDARY_CHANNEL_REVERSED, -roll_control/2 + pitch_control/2) ;
 		pwOut[AILERON_SECONDARY_OUTPUT_CHANNEL] = temp ;
 		
-		temp = (long)pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, (long)yaw_control)*/ ;
+		temp = pwIn[RUDDER_INPUT_CHANNEL] /*+ REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, yaw_control)*/ ;
 		pwOut[RUDDER_OUTPUT_CHANNEL] = pulsesat( temp ) ;
 		
-		temp = (long)pwIn[THROTTLE_INPUT_CHANNEL] ;
+		temp = pwIn[THROTTLE_INPUT_CHANNEL] ;
 		pwOut[THROTTLE_OUTPUT_CHANNEL] =  pulsesat( temp ) ;
-	}
+#endif
 }
