917197cd6a9c3d8e41a51cf63f51790dbe455492
matthew coleman <uavflightdirector@gmail.com>
2010-12-02 19:30:47 +0000

    Removed old CAN interface libraries

--- CANInterface/indicators.h
+++
@@ -1,9 +0,0 @@
-void init_LEDs(void);
-
-inline void redLEDon(void);
-inline void redLEDoff(void);
-inline void greenLEDon(void);
-inline void greenLEDoff(void);
-
-void indicator_tick_callback(void);
-
