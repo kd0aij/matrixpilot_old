7a4a54b709a39dd2a90a5ddd7b95a948df5e3c7b
matthew coleman <uavflightdirector@gmail.com>
2012-02-02 22:54:46 +0000

    Working flexifunctions download via buffer

--- libFlexiFunctions/flexiFunctionServices.c
+++ libFlexiFunctions/flexiFunctionServices.c
@@ -11,34 +11,10 @@
 
 #include "../libUDB/events.h"
 #include "flexifunction_options.h"
-
-
-// This testing section of code only compiles if you set the C-Compiler to use the "Large memory code model"
-// In MPLAB IDE, select "Project / Build Options / Project", then select Tab MPLAB C30. Then select the
-// drop down menu called "Categores" and select "Memory Model". Tick "Large Code Model" instead of 
-// "Default Code Model". i.e. The test code will need more than 28K of ROM.
-#define MAVLINK_TEST_ENCODE_DECODE	0
-
-#if ( MAVLINK_TEST_ENCODE_DECODE == 0 )
-// The following Macro enables MAVLink packets to be sent in one call to the serial driver
-// rather than character by character.
-#define MAVLINK_SEND_UART_BYTES mavlink_serial_send
-#endif
-
-
-#include "../MAVLink/include/matrixpilot/mavlink.h"
-
 #include "flexiFunctionServices.h"
 
-// Include MAVlink
-//#include "../MAVLink/include/matrixpilot/version.h"
-//#define 	MAVLINK_MAX_PAYLOAD_LEN 	MAVLINK_MAX_DIALECT_PAYLOAD_SIZE
-//#include "../MAVLink/include/inttypes.h"
-//   #include "../MAVlink/include/mavlink_types.h"
-//#include "../MAVLink/include/matrixpilot_mavlink_bridge_header.h"
-//#include "../MAVlink/include/matrixpilot/mavlink.h"
-
 // Include MAVlink library for checksums
+#include "../MAVlink/include/mavlink_types.h"
 #include "../MAVlink/include/checksum.h"
 
 
@@ -46,38 +22,6 @@ void flexifunction_commit_buffer_crc( void );
 
 inline void flexiFunction_NAK( void ); 		// Called to flag a negative acknowlege
 inline void flexiFunction_ACK( void ); 		// Called to flag a positive acknowlege
-
-
-enum FLEXIFUNCTION_SERVICES_STATUS
-{
-	FLEXIFUNCTION_NOT_STARTED,
-	FLEXIFUNCTION_WAITING,
-	FLEXIFUNCTION_SEND_BUFFER_SPECIFIC,
-	FLEXIFUNCTION_SEND_BUFFER_ALL,
-	FLEXIFUNCTION_SENDING_BUFFER_ALL,
-	FLEXIFUNCTION_SEND_STATISTICS,
-	FLEXIFUNCTION_READ_EEPROM,
-	FLEXIFUNCTION_READING_EEPROM,
-	FLEXIFUNCTION_WRITE_EEPROM,
-	FLEXIFUNCTION_WRITING_EEPROM,
-	FLEXIFUNCTION_COMMIT_BUFFER,		// Request to commit buffer from mavlink
-	FLEXIFUNCTION_COMMITTING_BUFFER,	// Checksum done and waiting for actual commit
-	FLEXIFUNCTION_COMMITTED_BUFFER,		// Commit done, send acknowledge
-	FLEXIFUNCTION_COMMAND_ACKNOWLEGE,
-};
-
-
-enum FLEXIFUNCTION_COMMANDS
-{
-	FLEXIFUNCTION_COMMAND_NULL,
-	FLEXIFUNCTION_COMMAND_FUNCTIONS_ALL,
-	FLEXIFUNCTION_COMMAND_FUNCTION_SPECIFIC,
-	FLEXIFUNCTION_COMMAND_COMMIT_BUFFER,
-	FLEXIFUNCTION_COMMAND_READ_EEPROM,
-	FLEXIFUNCTION_COMMAND_WRITE_EEPROM,
-	FLEXIFUNCTION_COMMAND_RELOAD_FROM_EEPROM,
-	FLEXIFUNCTION_COMMAND_READ_SETTINGS_TO_BUFFER,
-};
 
 
 // Buffer of all flexifunction data including used register and funciton count
@@ -104,8 +48,6 @@ void flexiFunctionService(void)
 		break;
 	case FLEXIFUNCTION_COMMIT_BUFFER:
 		flexifunction_commit_buffer_crc();
-		break;
-	case FLEXIFUNCTION_COMMITTED_BUFFER:
 		break;
 	}
 }
@@ -191,35 +133,52 @@ void flexifunction_sending_buffer_all()
 // Write a function to the buffer
 void flexiFunction_write_buffer_function(functionSetting* pFuncSetting, unsigned int index)
 {
-//	flexifunction_ref_command	= MAVLINK_MSG_ID_FLEXIFUNCTION_COMMIT_BUFFER;
-//	flexifunction_ref_compID	= compID;
-//	flexifunction_ref_sysID		= sysID;
-
-	if(flexiFunctionState != FLEXIFUNCTION_WAITING)
+	if(index >=  FLEXIFUNCTION_MAX_FUNCS)
 	{
-		flexiFunction_NAK();
+		flexifunction_ref_result = 0;
+		flexiFunctionState = FLEXIFUNCTION_BUFFER_FUNCTION_ACKNOWLEDGE;
 		return;
-	}
-	
-	// Write the buffer function
-	
-	flexiFunction_ACK();
+	}	
+
+	memcpy(&flexiFunctionBuffer.flexiFunction_data[index], pFuncSetting, sizeof(functionSetting));
+			
+	flexifunction_ref_result = 1;
+	flexiFunctionState = FLEXIFUNCTION_BUFFER_FUNCTION_ACKNOWLEDGE;
 	return;
 }
 
-void flexiFunction_commit_buffer(unsigned int checksum)
+
+// Write a functions count to the buffer
+void flexiFunction_write_functions_count(unsigned int funcCount)
+{
+	if(funcCount >= FLEXIFUNCTION_MAX_FUNCS)
+	{
+		flexifunction_ref_result = 0;
+		flexiFunctionState = FLEXIFUNCTION_SIZES_ACKNOWLEDGE;
+		return;
+	}
+	
+	flexiFunctionBuffer.flexiFunctionsUsed = funcCount;
+	flexifunction_ref_result = 1;
+	flexiFunctionState = FLEXIFUNCTION_SIZES_ACKNOWLEDGE;
+	return;
+}
+
+// Get functions count from buffer
+unsigned int flexiFunction_get_functions_count( void )
+{
+	return flexiFunctionBuffer.flexiFunctionsUsed;
+}
+
+
+void flexiFunction_commit_buffer()
 {
 	if(flexiFunctionState != FLEXIFUNCTION_WAITING)	
 	{
 		flexifunction_ref_result = 0;
-		flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEGE;
+		flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEDGE;
 		return;
 	}
-
-	flexifunction_ref_command	= MAVLINK_MSG_ID_FLEXIFUNCTION_COMMAND;
-	flexifunction_ref_checksum	= checksum;
-//	flexifunction_ref_compID	= compID;
-//	flexifunction_ref_sysID		= sysID;
 
 	if(flexiFunctionBuffer.flexiFunctionsUsed > FLEXIFUNCTION_MAX_REGS)
 	{
@@ -232,13 +191,13 @@ void flexiFunction_commit_buffer(unsigned int checksum)
 	flexiFunctionState = FLEXIFUNCTION_COMMIT_BUFFER;
 }
 
-
+// A place to do checksums if it is ever implemented
 void flexifunction_commit_buffer_crc( void )
 {
-	if(flexifunction_ref_checksum == crc_calculate( (uint8_t*) &flexiFunctionBuffer, sizeof(flexiFunctionBuffer) ) )
-		flexiFunctionState = FLEXIFUNCTION_COMMITTING_BUFFER;
-	else
-		flexiFunction_NAK();
+//	if(flexifunction_ref_checksum == crc_calculate( (uint8_t*) &flexiFunctionBuffer, sizeof(flexiFunctionBuffer) ) )
+	flexiFunctionState = FLEXIFUNCTION_COMMITTING_BUFFER;
+//	else
+//		flexiFunction_NAK();
 }
 
 
@@ -246,12 +205,7 @@ void flexifunction_commit_buffer_crc( void )
 void flexiFunction_commit_buffer_check()
 {
 	if(flexiFunctionState != FLEXIFUNCTION_COMMITTING_BUFFER) return;
-/*
-	pcompRef->pComponentData->numberRegs = flexiFunctionBuffer.componentData.numberRegs;
-	pcompRef->pComponentData->numberFuncs = flexiFunctionBuffer.componentData.numberFuncs;
-	unsigned int byteSize = flexiFunctionBuffer.componentData.numberFuncs * pcompRef->functionSize;
-	memcpy(pcompRef->pFunctionData,  flexiFunctionBuffer.flexiFunction_data, byteSize );
-*/
+
 	unsigned int index;
 	functionSetting* pTarget;
 	functionSetting* pSource;
@@ -266,6 +220,8 @@ void flexiFunction_commit_buffer_check()
 
 	flexiFunctionsUsed = flexiFunctionBuffer.flexiFunctionsUsed;
 
+	flexifunction_ref_command = FLEXIFUNCTION_COMMAND_COMMIT_BUFFER;
+
 	flexiFunction_ACK();
 }
 
@@ -273,52 +229,12 @@ void flexiFunction_commit_buffer_check()
 inline void flexiFunction_NAK( void )
 {
 	flexifunction_ref_result = 0;
-	flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEGE;
+	flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEDGE;
 }
 
 // Called to flag a positive acknowlege
 inline void flexiFunction_ACK( void )
 {
 	flexifunction_ref_result = 1;
-	flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEGE;
-}
-
-
-// Parse flexifunction specific messages
-void flexiFunctionReceiveParser(mavlink_message_t* msg)
-{
-	switch (msg->msgid)
-	{
-	    case MAVLINK_MSG_ID_FLEXIFUNCTION_SET:
-	    {
-			// Do nothing with this funciton since it is obsolete
-			// Must keep function defined to activate flexifunction mavlink libraries
-		}
-		break;
-	    case MAVLINK_MSG_ID_FLEXIFUNCTION_BUFFER_FUNCTION:
-	    {
-	        // decode
-			//send_text((unsigned char*)"Param Set\r\n");
-	        mavlink_flexifunction_buffer_function_t packet;
-	        mavlink_msg_flexifunction_buffer_function_decode(msg, &packet);
-
-//	        if (mavlink_check_target(packet.target_system,packet.target_component)) break ;
-
-			functionSetting fSetting;
-	
-			fSetting.functionType = packet.function_type;
-			fSetting.setValue = packet.Action;
-			fSetting.dest = packet.out_index;
-			if(packet.settings_data[0] != 's') return;
-			memcpy(&fSetting.data, &packet.settings_data[1], sizeof(functionData));
-
-			flexiFunction_write_buffer_function(&fSetting, packet.func_index);
-		}
-		break;
-	} // end case
-}
-
-
-void flexiFunctionTransmitService(mavlink_channel_t chan)
-{
+	flexiFunctionState = FLEXIFUNCTION_COMMAND_ACKNOWLEDGE;
 }
--- libFlexiFunctions/flexiFunctionServices.h
+++ libFlexiFunctions/flexiFunctionServices.h
@@ -5,7 +5,46 @@
 #include "../libUDB/events.h"
 #include "../libUDB/libUDB_internal.h"
 #include "flexifunction_options.h"
-#include "../MAVlink/include/mavlink_types.h"
+
+// References for sending acknowledge replies
+extern unsigned int flexifunction_ref_result;
+extern unsigned int flexifunction_ref_index;
+extern unsigned int flexifunction_ref_command;
+
+// State of the flexifunction services
+extern unsigned int flexiFunctionState;
+
+enum FLEXIFUNCTION_SERVICES_STATUS
+{
+	FLEXIFUNCTION_NOT_STARTED,
+	FLEXIFUNCTION_WAITING,
+	FLEXIFUNCTION_SEND_BUFFER_SPECIFIC,
+	FLEXIFUNCTION_SEND_BUFFER_ALL,
+	FLEXIFUNCTION_SENDING_BUFFER_ALL,
+	FLEXIFUNCTION_SEND_STATISTICS,
+	FLEXIFUNCTION_READ_EEPROM,
+	FLEXIFUNCTION_READING_EEPROM,
+	FLEXIFUNCTION_WRITE_EEPROM,
+	FLEXIFUNCTION_WRITING_EEPROM,
+	FLEXIFUNCTION_COMMIT_BUFFER,		// Request to commit buffer from mavlink
+	FLEXIFUNCTION_COMMITTING_BUFFER,	// Checksum done and waiting for actual commit
+	FLEXIFUNCTION_COMMIT_ACKNOWLEDGE,	// Commit done, send acknowledge
+	FLEXIFUNCTION_COMMAND_ACKNOWLEDGE,
+	FLEXIFUNCTION_BUFFER_FUNCTION_ACKNOWLEDGE,
+	FLEXIFUNCTION_SIZES_ACKNOWLEDGE,
+};
+
+enum FLEXIFUNCTION_COMMANDS
+{
+	FLEXIFUNCTION_COMMAND_NULL,
+	FLEXIFUNCTION_COMMAND_FUNCTIONS_ALL,
+	FLEXIFUNCTION_COMMAND_FUNCTION_SPECIFIC,
+	FLEXIFUNCTION_COMMAND_COMMIT_BUFFER,
+	FLEXIFUNCTION_COMMAND_READ_EEPROM,
+	FLEXIFUNCTION_COMMAND_WRITE_EEPROM,
+	FLEXIFUNCTION_COMMAND_RELOAD_FROM_EEPROM,
+	FLEXIFUNCTION_COMMAND_READ_SETTINGS_TO_BUFFER,
+};
 
 
 typedef struct tagNVMEM_FLEXIFUNCTION_DATA
@@ -19,11 +58,6 @@ void flexiFunctionServiceInit(void);
 
 // Trigger the flexifunction low priority service to run
 void flexiFunctionServiceTrigger(void);
-
-void flexiFunctionTransmitService(mavlink_channel_t chan);
-
-// Parse flexifunction specific messages
-void flexiFunctionReceiveParser(mavlink_message_t* msg);
 
 // Called by flexifunction loop to test for a new set of functions available
 void flexiFunction_commit_buffer_check( void );
@@ -39,6 +73,15 @@ void flexiFunction_write_nv_memory(void);
 
 // Load function settings from nv memory, test checksum and then commit
 void flexiFunction_load_nv_memory(void);
+
+// Write a function to the buffer
+void flexiFunction_write_buffer_function(functionSetting* pFuncSetting, unsigned int index);
+
+// Write a functions count to the buffer
+void flexiFunction_write_functions_count(unsigned int funcCount);
+
+// Get functions count from buffer
+unsigned int flexiFunction_get_functions_count( void );
 
 #endif 	//FLEXIFUNCTION_SERVICES
 
