c5900d959791a6be42a3a6ed76c697d4b5cae12d
matthew coleman <uavflightdirector@gmail.com>
2012-07-21 17:09:26 +0000

    libFlexiFunctions - Modified mixer and settings to fit post_mix processing.

--- libFlexiFunctions/flexiMixer.c
+++ libFlexiFunctions/flexiMixer.c
@@ -16,45 +16,32 @@ fractional flexiFunction_registers[FLEXIFUNCTION_MAX_REGS];
 //
 void preMix( void )
 {
-// Bring RMAX scaled input controls into registers.
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_ROLL)] 		= in_cntrls[IN_CNTRL_ROLL];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_PITCH)] 		= in_cntrls[IN_CNTRL_PITCH];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_THROTTLE)] 	= in_cntrls[IN_CNTRL_THROTTLE];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_YAW)] 		= in_cntrls[IN_CNTRL_YAW];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_CAMBER)] 	= in_cntrls[IN_CNTRL_CAMBER];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_BRAKE)] 		= in_cntrls[IN_CNTRL_BRAKE];
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_FLAP)] 		= in_cntrls[IN_CNTRL_FLAP];
-
-	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_APCON_WAGGLE)]	= PWM_to_frac(waggle,0, false);
+// Bring RMAX scaled output controls into registers.
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_ROLL)] 		= out_cntrls[IN_CNTRL_ROLL];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_PITCH)] 		= out_cntrls[IN_CNTRL_PITCH];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_THROTTLE)] 	= out_cntrls[IN_CNTRL_THROTTLE];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_YAW)] 		= out_cntrls[IN_CNTRL_YAW];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_CAMBER)] 	= out_cntrls[IN_CNTRL_CAMBER];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_BRAKE)] 		= out_cntrls[IN_CNTRL_BRAKE];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_PWIN_FLAP)] 		= out_cntrls[IN_CNTRL_FLAP];
 //
-//	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_TRIM_POINT)] 		= udb_pwTrim[ROLL_INPUT_CHANNEL];
-//	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_TRIM_THROTTLE)] 	= udb_pwTrim[THROTTLE_INPUT_CHANNEL];
+	flexiFunction_registers[get_input_register_index_from_directory(VIRTUAL_INPUT_APCON_WAGGLE)]	= ap_cntrls[AP_CNTRL_WAGGLE];
 }
 
 
 void postMix( void )
 {
-	udb_pwOut[AILERON_LEFT_OUTPUT_CHANNEL] 	= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_AILERON_L)] );
-	udb_pwOut[ELEVATOR_OUTPUT_CHANNEL] 		= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_ELEVATOR)] );
-	udb_pwOut[RUDDER_OUTPUT_CHANNEL] 		= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_RUDDER)] );
-	udb_pwOut[AILERON_RIGHT_OUTPUT_CHANNEL] = udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_AILERON_R)] );
-	udb_pwOut[FLAP_LEFT_OUTPUT_CHANNEL] 	= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAP_L)] );
-	udb_pwOut[FLAP_RIGHT_OUTPUT_CHANNEL] 	= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAP_R)] );
-	udb_pwOut[FLAPMID_LEFT_OUTPUT_CHANNEL] 	= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAPMID_L)] );
-	udb_pwOut[FLAPMID_RIGHT_OUTPUT_CHANNEL] = udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAPMID_R)] );
-	udb_pwOut[SPOILER_OUTPUT_CHANNEL] 		= udb_servo_pulsesat( (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_SPOILER)] );
+	mixer_outputs[AILERON_LEFT_OUTPUT_CHANNEL] 	= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_AILERON_L)];
+	mixer_outputs[ELEVATOR_OUTPUT_CHANNEL] 		= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_ELEVATOR)];
+	mixer_outputs[RUDDER_OUTPUT_CHANNEL] 		= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_RUDDER)];
+	mixer_outputs[AILERON_RIGHT_OUTPUT_CHANNEL] = flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_AILERON_R)];
+	mixer_outputs[FLAP_LEFT_OUTPUT_CHANNEL] 	= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAP_L)];
+	mixer_outputs[FLAP_RIGHT_OUTPUT_CHANNEL] 	= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAP_R)];
+	mixer_outputs[FLAPMID_LEFT_OUTPUT_CHANNEL] 	= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAPMID_L)];
+	mixer_outputs[FLAPMID_RIGHT_OUTPUT_CHANNEL] = flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_FLAPMID_R)];
+	mixer_outputs[SPOILER_OUTPUT_CHANNEL] 		= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_SPOILER)];
 
-	int throttle = (int) flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_THROTTLE)];
-
-	if(throttle < udb_pwTrim[THROTTLE_INPUT_CHANNEL])
-		throttle = udb_pwTrim[THROTTLE_INPUT_CHANNEL];
-
-	if ( udb_pwIn[THROTTLE_INPUT_CHANNEL] == 0 )
-	{
-		throttle = 0 ;
-	}
-
-	udb_pwOut[THROTTLE_OUTPUT_CHANNEL]	= throttle;
+	mixer_outputs[THROTTLE_OUTPUT_CHANNEL] 		= flexiFunction_registers[get_output_register_index_from_directory(VIRTUAL_OUTPUT_THROTTLE)];
 }
 
 
--- libFlexiFunctions/flexifunction_options.h
+++ libFlexiFunctions/flexifunction_options.h
@@ -4,8 +4,8 @@
 // Fix this to your particular radio at 100% travel
 #define MIX_PWM_RANGE 800
 
-#define FLEXIFUNCTION_MAX_REGS 				60
-#define FLEXIFUNCTION_MAX_FUNCS				80
+#define FLEXIFUNCTION_MAX_REGS 				48
+#define FLEXIFUNCTION_MAX_FUNCS				70
 #define FLEXIFUNCTION_MAX_DIRECTORY_SIZE 	32
 #define FLEXIFUNCTION_MAX_DATA_SIZE 		1024
 
