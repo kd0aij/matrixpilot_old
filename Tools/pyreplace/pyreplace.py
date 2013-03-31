4a2810be3dfe4d979eed1a1e1e5bab346c543282
matthew coleman <uavflightdirector@gmail.com>
2013-03-31 23:23:47 +0000

    MatrixPilot-fbw-merge - Added python script for changing to intx_t types

---
+++ Tools/pyreplace/pyreplace.py
@@ -0,0 +1,122 @@
+'''
+Created on 31 mar 2013
+
+@author: Matt
+'''
+
+import re, os, sys
+
+class searchstr(object):
+    def __init__(self, find, replacewith):
+        self.find = find
+        self.replacewith = replacewith
+
+
+class replacer(object):
+    def __init__(self, rootdir, opts, searchstrings, predelim, postdelim):
+        self.rootdir = rootdir
+        self.opts = opts
+        self.sstr = searchstrings
+        self.predelim = predelim
+        self.postdelim = postdelim
+
+    def run(self):
+        
+        self.fileList = []
+
+        for directory in self.opts.directories:
+            path = os.path.join(self.rootdir, directory)
+
+            fileSize = 0
+            folderCount = 0    
+
+            for root, subFolders, files in os.walk(path):
+                folderCount += len(subFolders)
+                for filename in files:
+                    f = os.path.join(root,filename)
+                    fileSize = fileSize + os.path.getsize(f)
+                    #print(f)
+                    splt = filename.split(".")
+                    if(len(splt) == 2):
+                        if(splt[1] == "c"):
+                            self.fileList.append(f)
+                        if(splt[1] == "h"):
+                            self.fileList.append(f)
+                        if(splt[1] == "hpp"):
+                            self.fileList.append(f)
+            
+            for filepath in self.fileList:
+                fin = open(filepath, "r")
+                fout = open(filepath + ".txt", "w")
+                content = fin.readlines()
+                
+                for line in content:
+                    outline = line
+                    for sstr in self.sstr:
+                        for pre in self.predelim:
+                            for post in self.postdelim:
+        
+                                cmpstr = (pre + sstr.find + post)
+                                replacestr = (pre + sstr.replacewith + post)
+                                
+#                                if(outline.find("unsigned int") >= 0):
+#                                    print("found line with unsigned int")  
+#                                    if(filepath.find("airframe.h") >= 0):
+#                                        print("found airfram.h with unsigned int")
+#                                    if(outline.find(cmpstr) >= 0):
+#                                        print("found line with unsigned int that fits cmpstr")                                                                      
+                                
+#                                if(pre == "unsigned int"):
+#                                prereplace = outline
+                                outline = outline.replace(cmpstr, replacestr)
+#                                if(prereplace == outline):
+#                                    print("replace failure")
+                    fout.write(outline)
+                fin.close
+                print("Finished file: " + filepath)
+            print("Complete")
+
+
+    
+
+
+if __name__ == '__main__':
+
+    from optparse import OptionParser
+    parser = OptionParser("pypolar.py [options]")
+
+    parser.add_option("--directory", dest="directories", action='append', help="search directory")
+
+    parser.add_option("--recursive", dest="recursive", help="File search is recursive", action='store_true', default=False)
+
+    parser.add_option("--test", dest="test", help="File search is recursive", action='store_true', default=False)
+
+    (opts, args) = parser.parse_args()
+    
+    sstr = []
+    pre = []
+    post = []
+    
+    sstr.append(searchstr("long long", "int64_t"))
+    sstr.append(searchstr("unsigned long", "uint32_t"))
+    sstr.append(searchstr("long", "int32_t"))
+    sstr.append(searchstr("unsigned int", "uint16_t"))
+    sstr.append(searchstr("int", "int16_t"))
+    sstr.append(searchstr("unsigned short", "uint16_t"))
+    sstr.append(searchstr("short", "int16_t"))
+    sstr.append(searchstr("unsigned char", "uint8_t"))
+    
+    pre.append(" ")
+    pre.append("(")
+#    pre.append("\t")
+    pre.append("\t")
+    
+    post.append(" ")
+    post.append(")")
+    post.append("\t")
+    post.append("*")
+    
+    rootdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..")
+
+    rep = replacer(rootdir, opts, sstr, pre, post)
+    rep.run()
