075c9df6c6c9271bae0a4ef976ea7a19336fcd57
William Premerlani <wpremerlani@gmail.com>
2013-04-16 23:43:52 +0000

    MP_WJP_research branch : added a routine to rotate a 32 bit 2D vector

--- libDCM/mathlibNAV.c
+++ libDCM/mathlibNAV.c
@@ -138,6 +138,17 @@ void rotate_2D_vector_by_vector( int16_t vector[2] , int16_t rotate[2])
 	return ;
 }
 
+void rotate_2D_long_vector_by_vector ( int32_t vector[2] , int16_t rotate[2] )
+{
+//	same as rotate_2D_vector_by_vector, except the first vector is 32 bits
+	int32_t newx , newy ;
+	newx = long_scale ( vector[0] , rotate[0] ) - long_scale ( vector[1] , rotate[1] ) ;
+	newy = long_scale ( vector[0] , rotate[1] ) + long_scale ( vector[1] , rotate[0] ) ;
+	vector[0] = newx ;
+	vector[1] = newy ;
+	return ;
+}
+
 void rotate_2D_vector_by_angle( int16_t vector[2] , int8_t angle)
 {
 //	rotate the vector by angle,
@@ -365,6 +376,43 @@ uint16_t vector3_normalize( int16_t result[] , int16_t input[] )
 
 int32_t long_scale ( int32_t arg1 , int16_t arg2 )
 {
+	//	returns arg1*arg2/RMAX
+	//  usually this is used where arg2 is a Q14.2 fractional number
+	int8_t sign_result = 1;
+	int32_t product ;
+	union longww accum ;
+	union longww arg1ww ;
+	arg1ww.WW = arg1 ;
+	if ( arg1ww._.W1 < 0 )
+	{
+		sign_result = - sign_result ;
+		arg1ww.WW = -arg1ww.WW ;
+	}
+	if( arg2 < 0 )
+	{
+		sign_result = - sign_result ;
+		arg2 = -arg2 ;
+	}
+	product = __builtin_mulss( arg2 , arg1ww._.W1 ) ;
+	accum.WW =  __builtin_mulss( arg2 , arg1ww._.W0 ) ;
+	accum._.W0 = accum._.W1 ;
+	accum._.W1 = 0 ;
+	product += accum.WW ;
+	product <<= 2 ;
+	if ( sign_result > 0 )
+	{
+		return product ;
+	}
+	else
+	{
+		return -product ;
+	}
+
+}
+
+/*
+int32_t long_scale ( int32_t arg1 , int16_t arg2 )
+{
 	union longlongLL LLaccumulator ;
 	union longlongLL arg1LL , arg2LL ;
 	arg1LL.LL = (int64_t) arg1 ;
@@ -374,6 +422,7 @@ int32_t long_scale ( int32_t arg1 , int16_t arg2 )
 	LLaccumulator.LL <<= 2 ;
 	return LLaccumulator._.L1 ;
 }
+*/
 
 
  
